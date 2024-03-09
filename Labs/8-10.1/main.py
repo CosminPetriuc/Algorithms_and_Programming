01770823a4b434f2982d251fba39611e6d082b43
CosminPetriuc <122042873+CosminPetriuc@users.noreply.github.com>
2024-03-09 04:18:38 +0200

    Delete Labs/8-10.1 directory

--- Labs/8-10.1/main.py
+++
@@ -1,8 +0,0 @@
-from Domain.My_Vector import MyVector
-from Repo.VectorRepo import VectorRepository
-from ui.UI import VectorUI
-
-# Main program
-if __name__ == "__main__":
-    vector_ui = VectorUI()
-    vector_ui.run()
