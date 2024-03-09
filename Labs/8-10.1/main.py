22f6b66c8c9722134ee7cc0d5cb667175627ba3b
CosminPetriuc <122042873+CosminPetriuc@users.noreply.github.com>
2024-03-09 04:10:06 +0200

    Add files via upload

---
+++ Labs/8-10.1/main.py
@@ -0,0 +1,8 @@
+from Domain.My_Vector import MyVector
+from Repo.VectorRepo import VectorRepository
+from ui.UI import VectorUI
+
+# Main program
+if __name__ == "__main__":
+    vector_ui = VectorUI()
+    vector_ui.run()
